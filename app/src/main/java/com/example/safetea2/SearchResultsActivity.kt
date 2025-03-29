539ce45851d129e8a79d83668584bfad1d2c830f
alyss <alyssaduranovic@gmail.com>
2025-03-29 13:25:13 -0700

    urmom

--- app/src/main/java/com/example/safetea2/SearchResultsActivity.kt
+++ app/src/main/java/com/example/safetea2/SearchResultsActivity.kt
@@ -3,6 +3,7 @@ package com.example.safetea2
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 
 class SearchResultsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,7 @@ class SearchResultsActivity : AppCompatActivity() {
 
         val searchQuery = intent.getStringExtra("SEARCH_QUERY") ?: "No query"
 
-        val resultsTextView: TextView = findViewById(R.id.resultsTextView)
+        val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
         resultsTextView.text = "Results for: $searchQuery"
     }
 }
